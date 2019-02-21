7e745e988417a586a22689bb5a2980a8508bebb5
wpsmith <t@wpsmith.net>
2019-02-20 20:54:38 -0500

    Update namespace/docs

--- src/AbstractRoleManager.php
+++ src/AbstractRoleManager.php
@@ -11,21 +11,21 @@
  *
  * @package    WPS\Roles
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Roles;
+namespace WPS\WP\Roles;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Roles\AbstractRoleManager' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\AbstractRoleManager' ) ) {
 
 	/**
 	 * Abstract Role Manager template.
--- src/RoleManagerVIP.php
+++ src/RoleManagerVIP.php
@@ -11,20 +11,21 @@
  *
  * @package    WPS\Roles
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
-namespace WPS\Roles;
+
+namespace WPS\WP\Roles;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Roles\RoleManagerVIP' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\RoleManagerVIP' ) ) {
 /**
  * VIP implementation of the Role Manager.
  */
